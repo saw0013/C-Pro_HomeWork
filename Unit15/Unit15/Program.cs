c59985a5d5f83d23da1fb1aa8ba3322efafb907b
saw0013 <vadim_nonik2007@mail.ru>
2023-04-01 14:29:30 +0400

    Unit15

---
+++ Unit15/Unit15/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Unit15
+{
+    internal class Program
+    {
+        static Random random = new Random();
+
+        /// <summary>
+        /// Асинхронный метод
+        /// </summary>
+        /// <param name="name"></param>
+        async static void AsyncMethod(object name)
+        {
+            int id = (int)Task.CurrentId; // id выполняющейся задачи
+
+            await Task.Delay(random.Next(100, 1000)); // Рандомная задержка
+            Console.WriteLine($"Задача_{id} : Имя Задачи {name}"); // Выводим информацию в консоль
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количества запускаемых потоков...");
+            int countTriggeredThreads = int.Parse(Console.ReadLine()); // Количество запускаемых потоков
+
+            for (int i = 0; i < countTriggeredThreads; i++)
+            {
+                var task = new Task(AsyncMethod, $"AsyncMethod_{i}"); // Создаем задачу
+                task.Start(); // Запускаем задачу
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
