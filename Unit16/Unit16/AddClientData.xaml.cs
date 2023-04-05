76c86e68bb5e789ef1c749fd23d68e117544c889
saw0013 <vadim_nonik2007@mail.ru>
2023-04-05 20:22:36 +0400

    Unit15_help_error

---
+++ Unit16/Unit16/AddClientData.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Unit16
+{
+    /// <summary>
+    /// Логика взаимодействия для AddClientData.xaml
+    /// </summary>
+    public partial class AddClientData : Window
+    {
+        private AddClientData() { InitializeComponent(); }
+
+        public AddClientData(DataRow row) : this()
+        {
+            CancelButton.Click += delegate { this.DialogResult = false; };
+            AddButton.Click += delegate
+            {
+                row["lastName"] = lastName.Text;
+                row["name"] = name.Text;
+                row["middleName"] = middleName.Text;
+                row["numberPhone"] = numberPhone.Text;
+                row["emailAdress"] = Email.Text;
+                this.DialogResult = !false;
+            };
+
+        }
+    }
+}
---
+++ Unit16/Unit16/MainWindow.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Data;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Unit16
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        SqlConnection con1;
+        SqlDataAdapter da;
+        DataTable dt;
+        DataRowView row;
+
+        public MainWindow()  {InitializeComponent(); Preparing();}
+
+        private void Preparing()
+        {
+            #region Init LocalDB
+            var connectionStringBuilder = new SqlConnectionStringBuilder
+            {
+                DataSource = @"(localdb)\MSSQLLocalDB", 
+                InitialCatalog = "msdb",
+                UserID = "Admin",
+                Password = "Password",
+            };
+
+            con1 = new SqlConnection(connectionStringBuilder.ConnectionString);
+            dt = new DataTable();
+            da = new SqlDataAdapter();
+
+            con1.StateChange +=
+                (s, e) => {
+                    MessageBox.Show($@"{nameof(con1)} в состоянии:" +
+                    $" {(s as SqlConnection).State}");
+                };
+
+            try
+            {
+                con1.Open(); 
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message); 
+            }
+            finally
+            {
+                con1.Close();
+            }
+            #endregion
+
+            #region Init MS Access
+
+            #endregion
+        }
+
+
+        private void MenuItemAddClientClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MenuItemDeleteClientClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MenuItemAddProductClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MenuItemDeleteProductClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void GVCurrentCellChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void GVCellEditEnding(object sender, DataGridCellEditEndingEventArgs e)
+        {
+
+        }
+    }
+}
