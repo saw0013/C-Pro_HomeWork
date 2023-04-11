d279e1fbafe24c76482d2cbb2c93a9aac513f21b
saw0013 <vadim_nonik2007@mail.ru>
2023-04-11 22:37:07 +0400

    Unit19

---
+++ Unit19/Unit19/Controllers/ContactsController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Unit19.Contolers
+{
+    public class ContactsController : Controller
+    {
+        private readonly List<Contact> contacts;
+
+        public ContactsController()
+        {
+            contacts = new List<Contact>
+        {
+            new Contact { ID = 1, LastName = "Иванов", FirstName = "Иван", MiddleName = "Иванович", PhoneNumber = "+7 (123) 456-7890", Address = "Москва, ул. Пушкина, д. 1", Description = "Старший менеджер" },
+            new Contact { ID = 2, LastName = "Петров", FirstName = "Петр", MiddleName = "Петрович", PhoneNumber = "+7 (123) 456-7891", Address = "Москва, ул. Лермонтова, д. 2", Description = "Младший менеджер" },
+            new Contact { ID = 3, LastName = "Сидоров", FirstName = "Сидор", MiddleName = "Сидорович", PhoneNumber = "+7 (123) 456-7892", Address = "Москва, ул. Гоголя, д. 3", Description = "Консультант" },
+        };
+        }
+
+        public IActionResult Index()
+        {
+            return View(contacts);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var contact = contacts.FirstOrDefault(c => c.ID == id);
+            return View(contact);
+        }
+    }
+}
---
+++ Unit19/Unit19/Models/Contact.cs
@@ -0,0 +1,10 @@
+﻿public class Contact
+{
+    public int ID { get; set; }
+    public string LastName { get; set; }
+    public string FirstName { get; set; }
+    public string MiddleName { get; set; }
+    public string PhoneNumber { get; set; }
+    public string Address { get; set; }
+    public string Description { get; set; }
+}
